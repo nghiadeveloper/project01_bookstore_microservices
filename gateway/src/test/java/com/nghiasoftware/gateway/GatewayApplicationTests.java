b44379b66033a954d2daa8ef75ca6e7d97821304
nghiadeveloper <hieunghia.dev@gmail.com>
2025-06-22 11:09:49 +0700

    Xây dựng service_gateway

---
+++ gateway/src/test/java/com/nghiasoftware/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nghiasoftware.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
