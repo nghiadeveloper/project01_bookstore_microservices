e8dc4e9ecd80cf86d7b8da5b8e7fa8c4d70af95e
nghiadeveloper <hieunghia.dev@gmail.com>
2025-06-29 11:07:59 +0700

    Xây dựng Servive_Eureka_Server

---
+++ service_eureka_server/src/test/java/com/nghiasoftware/service_eureka_server/ServiceEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nghiasoftware.service_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
